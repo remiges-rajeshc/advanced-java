2a6166d51a6265ecd98e88eb1fe5ff8c00f11819
rajeshc <rajesh.chowdhury@remiges.tech>
2024-03-28 12:36:46 +0530

    multithread task

---
+++ src/test/java/com/remigest/demotech/DemotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remigest.demotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
